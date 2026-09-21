81935114ba45b3ea0b218daba989d6f033f514bf
Utkarsh Bhiogade <37672850+utkarshb1@users.noreply.github.com>
2026-09-22 00:22:56 +0530

    Cache-bust transparent Ganesha emblem

--- components/indian-ornaments.tsx
+++ components/indian-ornaments.tsx
@@ -3,7 +3,7 @@ import { BASE_PATH } from '@/lib/base-path'
 export function GaneshEmblem() {
   return (
     <img
-      src={`${BASE_PATH}/ganesha-emblem-clean.png`}
+      src={`${BASE_PATH}/ganesha-emblem-transparent.png`}
       alt="Lord Ganesha"
       className="h-24 w-24 object-contain sm:h-28 sm:w-28"
     />
